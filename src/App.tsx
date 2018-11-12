05136715f5cf8ac35eea9e6870d4f313d98908d0
Adam Richardson <adam@example.com>
2018-11-12 07:25:23 -0500

    Added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
 
-import logo from './logo.svg';
+import styled from "styled-components";
+
+import logo from "./logo.svg";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+`;
+
+const AppTitle = styled.h1`
+  font-size: 1.5em;
+`;
+
+const AppLogo = styled.img`
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <AppContainer>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppTitle>Welcome to React</AppTitle>
+        </AppHeader>
+        <AppIntro>
           To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        </AppIntro>
+      </AppContainer>
     );
   }
 }
